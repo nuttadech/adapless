136349692dd6e89f123353214ffdc7ea1856d01b
nuttadech <NDTechnology>
2014-08-08 10:37:01 +0700

    adapless responsive
    responsive framework for enterprise

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>adaptless.co</title>
+	<meta name=”description” content=”adapless.co is a framework for enterprise with less and full responsive function.”>
+	<meta name=”keywords” content=”adapless,co,Responsive framework” />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main template -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- Less main stylesheet -->
+	<link rel="stylesheet/less" href="assets/css/style.less">
+	<!-- Responsive -->
+	<!-- Animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<script src="assets/js/modernizr.js"></script>
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="plugin/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!--!!Less!!-->
+	<script src="assets/js/less-1.7.3.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+/*
+ * adaptless.co :: Fully responsive function for enterprise.
+ * Web-Based Instruction PHP Programming :: ND TECHNOLOGY (Thailand)
+ * Web-Design ::  ND TECHNOLOGY (Thailand )
+ * Copyright (C) 2014
+ * Contact :: nuttadech@nd.co.th 
+ * This is the integration file for PHP (versions 5)
+*/
+	require_once 'inc/header.php';
+?>
+<body>
+<!-- //HEADER SECTION// -->
+	<header>
+		
+		<nav class="header--nav">
+		<h1 class="header--logo link animated shake"><a href="#" rel="nofollow">Adaptless<span class="yellow">.</span>co</a></h1>
+			<ul class="header--nav__menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Feature</a></li>
+				<li><a href="#">Doc</a></li>
+				<li><a href="#">Support</a></li>
+				<li><a href="#">Download</a></li>
+			</ul>
+		</nav>
+	</header>
+<!-- //HEADER SECTION// -->
+<!-- //Container// -->
+	<section class="container ">
+		<article class="newrow">
+			<div class="col-12">
+				<h3 class="blue">Col-12</h3>
+				<img src="http://placehold.it/1170x100" />
+			</div>
+		</article>
+		<article class="newrow">
+			<div class="col-11">
+				<h3 class="blue">Col-11</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-11">
+				<h3 class="blue">Col-11</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+		</article>	
+		<article class="newrow">
+			<div class="col-10">
+				<h3 class="blue">Col-10</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-10">
+				<h3 class="blue">Col-10</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-10">
+				<h3 class="blue">Col-10</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+		</article>
+		<article class="newrow">
+			<div class="col-9">
+				<h3 class="blue">Col-9</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-9">
+				<h3 class="blue">Col-9</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-9">
+				<h3 class="blue">Col-9</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-9">
+				<h3 class="blue">Col-9</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+		</article>	
+		<article class="newrow">
+			<div class="col-8">
+				<h3 class="blue">Col-8</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-8">
+				<h3 class="blue">Col-8</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-8">
+				<h3 class="blue">Col-8</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-8">
+				<h3 class="blue">Col-8</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+			<div class="col-8">
+				<h3 class="blue">Col-8</h3>
+				<img src="http://placehold.it/585x100" />
+			</div>
+		</article>	
+		<article class="newrow">
+			<h3>Element</h3>
+			<button class="black-button">BLACK BUTTON</button>
+			<button class="red-button">RED BUTTON</button>
+		</article>	
+	</section>
+	
+</body>
+</html>
+
+
+
+
